1a4a3d4219b79de15229ea7ff28df13f035d42bc
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-03-28 22:32:13 -0300

    new component

--- traveler/src/components/PlaceDetails/PlaceDetails.jsx
+++ traveler/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+import useStyles from './styles';
 
-const PlaceDetails =()=>{
+const PlaceDetails =({ place })=>{
+    const classes = useStyles();
     return(
-        <h1>PlaceDetails</h1>
+        <Card elevation={6}>
+            <CardMedia 
+            style={{ heinght: 350 }}
+            image={place.photo ? place.photo.images.large.url : 'https://www.instock.nl/app/uploads/2016/07/RubendeRuijterInstockUT-25.jpg'}
+            title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant='h5'>{place.name}</Typography>
+            </CardContent>
+        </Card>
     );
 }
 export default PlaceDetails;
